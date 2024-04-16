0f8291078cf5bd93082cae9f1f2584c4aa52d892
AR ChuChu <mjssdn95@gmail.com>
2024-04-16 19:10:32 +0000

    sandbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
-import Header from './components/Header';
-import AllTasksPage from './pages/AllTasksPage';
-import NewTasksPage from './pages/NewTasksPage';
-import DoingTasksPage from './pages/DoingTasksPage';
-import DoneTasksPage from './pages/DoneTasksPage';
-import TaskDetailPage from './pages/TaskDetailPage';
-import CreateTaskPage from './pages/CreateTaskPage';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/system';
-import SearchTask from './components/SearchTask';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import Header from "./components/Header";
+import AllTasksPage from "./pages/AllTasksPage";
+import NewTasksPage from "./pages/NewTasksPage";
+import DoingTasksPage from "./pages/DoingTasksPage";
+import DoneTasksPage from "./pages/DoneTasksPage";
+import TaskDetailPage from "./pages/TaskDetailPage";
+import CreateTaskPage from "./pages/CreateTaskPage";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/system";
 
 const CustomButton = styled(Button)({
-  margin: '0 10px',
+  margin: "0 10px",
 });
 
 function App() {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const handleSearch = (value) => {
     setSearchValue(value);
@@ -26,17 +31,63 @@ function App() {
     <Router>
       <div>
         <Header onSearch={handleSearch} />
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-          <CustomButton component={Link} to="/" variant="contained" color="primary">All Tasks</CustomButton>
-          <CustomButton component={Link} to="/new" variant="contained" color="secondary">New</CustomButton>
-          <CustomButton component={Link} to="/doing" variant="contained" color="error">Doing</CustomButton>
-          <CustomButton component={Link} to="/done" variant="contained" color="success">Done</CustomButton>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          <CustomButton
+            component={Link}
+            to="/"
+            variant="contained"
+            color="primary"
+          >
+            All Tasks
+          </CustomButton>
+          <CustomButton
+            component={Link}
+            to="/new"
+            variant="contained"
+            color="secondary"
+          >
+            New
+          </CustomButton>
+          <CustomButton
+            component={Link}
+            to="/doing"
+            variant="contained"
+            color="error"
+          >
+            Doing
+          </CustomButton>
+          <CustomButton
+            component={Link}
+            to="/done"
+            variant="contained"
+            color="success"
+          >
+            Done
+          </CustomButton>
         </div>
         <Routes>
-          <Route path="/" element={<AllTasksPage searchValue={searchValue} />} />
-          <Route path="/new" element={<NewTasksPage searchValue={searchValue} />} />
-          <Route path="/doing" element={<DoingTasksPage searchValue={searchValue} />} />
-          <Route path="/done" element={<DoneTasksPage searchValue={searchValue} />} />
+          <Route
+            path="/"
+            element={<AllTasksPage searchValue={searchValue} />}
+          />
+          <Route
+            path="/new"
+            element={<NewTasksPage searchValue={searchValue} />}
+          />
+          <Route
+            path="/doing"
+            element={<DoingTasksPage searchValue={searchValue} />}
+          />
+          <Route
+            path="/done"
+            element={<DoneTasksPage searchValue={searchValue} />}
+          />
           <Route path="/tasks/:taskId" element={<TaskDetailPage />} />
           <Route path="/create-task" element={<CreateTaskPage />} />
           <Route path="*" element={<Navigate to="/" />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { styled } from '@mui/system';
-import SearchTask from './SearchTask';
-import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
-import { AppBar, Toolbar, Button } from '@mui/material';
+import React from "react";
+import { styled } from "@mui/system";
+import SearchTask from "./SearchTask";
+import { Link, useNavigate } from "react-router-dom"; // Import useNavigate instead of useHistory
+import { AppBar, Toolbar, Button } from "@mui/material";
 
-const FlexContainer = styled('div')({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  width: '100%',
+const FlexContainer = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
 });
 
 const Header = ({ onSearch }) => {
@@ -18,7 +18,14 @@ const Header = ({ onSearch }) => {
       <Toolbar>
         <FlexContainer>
           <div className="header-left">
-            <Button component={Link} to="/create-task" variant="contained" color="secondary">Create Task</Button>
+            <Button
+              component={Link}
+              to="/create-task"
+              variant="contained"
+              color="secondary"
+            >
+              Create Task
+            </Button>
           </div>
           <div className="header-right">
             <SearchTask onSearch={onSearch} />
